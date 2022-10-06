fd6784d504271ec08b353d92f693460515d1e63f
hoodti33-t <77997616+hoodti33-t@users.noreply.github.com>
2022-10-05 21:44:45 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 const SERVER_NAME = "sql5.freemysqlhosting.net";
-   const USERNAME = "sql5476951";
-   const PASSWORD = "imnkJpmp4H";
-   const DB = "sql5476951";
+   const USERNAME = "sql5524654";
+   const PASSWORD = "3pM1upx2ce";
+   const DB = "sql5524654";
 
 $conn = mysqli_connect(SERVER_NAME, USERNAME, PASSWORD, DB);
 ?>
